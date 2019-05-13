42822e30c359508144b5f43d3b61e4fa3e467460
Alessandro Fadini <afadini@widow-tll.labs.stonybrook.edu>
2019-05-13 13:41:15 -0400

    clarified script

--- notSynced/scripts/gitScript.sh
+++ notSynced/scripts/gitScript.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Welcome to the automated Aleks Git Commit Dotfiles Script"
 echo "Also known as AGCDS, a memorable acronym"
+echo "This only githubs what is inside of TLLdotfiles"
 read -p "Insert your Git Comment:" gitcomment
 cd ~/TLLdotfiles
 git add .
