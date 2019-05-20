f2d6e02b97fcb63b6c64748de9b35f0ec4066c25
Alessandro Fadini <afadini@riley-tll.labs.stonybrook.edu>
2019-05-20 11:52:01 -0400

    fixed syncDotFiles script

--- syncDotfiles.sh
+++ syncDotfiles.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 echo "Welcome to the Aleks DotFiles Sync System (ADSS)"
-echo "Hello, do you want to automatically process (batch process) all .WEBM video files in this folder (y/n)?"
-read confirmation
-if ["$confirmation" = "y"]
+echo "Hello, do you want copy all the dot files from github to this machine "
+read -p "(y/n)? " -n 1 -r
+echo #skip a line
+if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	echo "Just sit back and relax while your system configurations are being set up..."
+	echo # skip a line
 	cd ~/TLLdotfiles
 	rsync -v --exclude "syncDotfiles.sh" --exclude "README.md" --exclude "notSynced/" --exclude ".git/" . ~
 else
